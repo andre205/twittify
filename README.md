a8c7809c0a8efc4de60180b9f877aea6eb2165ba
Tyler Andrews <andre205@mail.chapman.edu>
2017-04-06 02:03:07 -0700

    Spotify search results directly from web API

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# twittify
+# Project Twittify
 
-Project Twittify<br>
 Implements Spotify and Twitter APIs to view tweets about songs.<br>
 Also, generate a playlist from the most tweeted songs of the day/week/year.
 
@@ -8,3 +7,11 @@ Resources:<br>
 https://github.com/thelinmichael/spotify-web-api-java<br>
 https://github.com/thelinmichael/spotify-web-api-node<br>
 https://facebook.github.io/react-native/docs/getting-started.html<br>
+
+
+
+JSON-java<br>
+https://github.com/stleary/JSON-java
+To install - copy the json-20160810 jar file to your jdk extensions folder<br>
+ie. C:\Program Files (x86)\Java\jdk1.8.0_73\jre\lib\ext<br>
+This will allow the program to import the JSON handler.
--- spotify_groundwork.java
+++ spotify_groundwork.java
@@ -1,66 +1,91 @@
-//FROM https://github.com/thelinmichael/spotify-web-api-java/blob/master/examples/GetAuthenticatedUserInformation.java
+import java.util.*;
+import java.io.*;
+import java.io.InputStream;
+import org.json.*;
 
 
-import com.wrapper.spotify.Api;
-import com.wrapper.spotify.models.AuthorizationCodeCredentials;
-import com.wrapper.spotify.models.User;
+//RETRIEVES A JSON FROM WEB API BASED ON SEARCH TERM
+public class spotify_groundwork {
 
-/**
- * This example shows how to get information about the user that is 'connected' to the
- * access token. The methods used (api.authorizationCodeGrant and api.getMe) are synchronous, but are
- * available asynchronously as well. The scopes necessary for this example are 'user-read-private'
- * and 'user-read-email'.
- *
- * The authorization flow used is documented in detail at
- * https://developer.spotify.com/spotify-web-api/authorization-guide/#authorization_code_flow
- *
- * Details about requesting the current user's information is documented at
- * https://developer.spotify.com/spotify-web-api/get-users-profile/ in the
- * "Authorization Code" section.
- */
-public class CurrentUser {
+  public static void main(String[] args){
 
-  public static void main(String[] args) {
+    //We can modify this string based on the search terms
+    //options -- album, track, artist
+    //https://developer.spotify.com/web-api/search-item/
 
-    /* Application details necessary to get an access token */
-    final String clientId = "<insert client id>";
-    final String clientSecret = "<insert client secret";
-    final String code = "<insert code>";
-    final String redirectUri = "<insert redirect URI>";
-
-    /* Create a default API instance that will be used to make requests to Spotify */
-    final Api api = Api.builder()
-            .clientId(clientId)
-            .clientSecret(clientSecret)
-            .redirectURI(redirectUri)
-            .build();
-
+    String urltext = "https://api.spotify.com/v1/search?q=Sound+of+Silence&type=track";
+    java.net.URL url = null;
     try {
-
-      /* Retrieve an access token */
-      final AuthorizationCodeCredentials authorizationCodeCredentials = api.authorizationCodeGrant(code).build().get();
-
-      /* The token response contains a refresh token, an accesstoken, and some other things.
-       * We only need the access token to retrieve the user's information.
-       */
-      final String accessToken = authorizationCodeCredentials.getAccessToken();
-
-      /* Retrieve information about the user.
-      * The amount of information that is set on the User object depends on the scopes that
-      * the user has allowed the application to read.
-      * Read about which scopes that are available on
-      * https://developer.spotify.com/spotify-web-api/get-users-profile/
-      */
-      final User currentUser = api.getMe().accessToken(accessToken).build().get();
-
-      /* Use the information about the user */
-      System.out.println("URI to currently logged in user is: " + currentUser.getUri());
-      System.out.println("The currently logged in user comes from: " + currentUser.getCountry());
-      System.out.println("You can reach this user at: " + currentUser.getEmail());
-
-    } catch (Exception e) {
-      System.out.println("Something went wrong.");
+      url = new java.net.URL(urltext);
     }
+    catch (Exception e) {
+      System.out.println("make url error");
+    }
+
+    InputStream input = null;
+    try {
+        input = url.openStream();
+    }
+    catch (Exception e) {
+      System.out.println("open stream error");
+    }
+
+    java.io.BufferedReader reader = null;
+    try {
+        reader = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+    }
+    catch (Exception e) {
+      System.out.println("create reader error");
+    }
+
+    String entireJSON = "";
+    String s = "test";
+    try {
+        while(s != null)
+        {
+            s = reader.readLine();
+            //PRINT ENTIRE JSON
+            //System.out.println(s);
+            if (s != null)
+              entireJSON += s;
+        }
+    }
+    catch (Exception e) {
+        System.out.println("reader parsing error");
+    }
+
+    //Create object containting all results
+    JSONObject APIresponse = new JSONObject(entireJSON);
+
+    //actual results one layer in for some reason
+    JSONObject tracks = APIresponse.getJSONObject("tracks");
+
+    //split search results into a JSON array
+    JSONArray items = tracks.getJSONArray("items");
+    //iterate through the array and display data on each result
+    for (int i = 0; i < items.length(); ++i)
+    {
+       String songname = items.getJSONObject(i).getString("name");
+       System.out.println(songname);
+    }
+
+
+    //JSONObject items = tracks.getJSONObject("items");
+
+    // JSONArray arr = APIresponse.getJSONArray("tracks");
+    // for (int i = 0; i < arr.length(); i++)
+    // {
+    //     JSONObject tracks = arr.getJSONObject(i);
+    //     JSONArray items = tracks.getJSONArray("items");
+    //     for (int j = 0; j < items.length(); j++)
+    //     {
+    //         String test = items.getJSONObject(j).getString("type");
+    //         System.out.println(test);
+    //     }
+    // }
+
+
+
 
   }
 }
