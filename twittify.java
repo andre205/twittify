9292d581e8d7f8a8b94875f3ca8db57ef661c8c8
Tyler Andrews <Tyler Andrews>
2017-04-06 14:42:57 -0700

    JSON Overhaul

--- twittify.java
+++ twittify.java
@@ -1,1 +1,9 @@
 //Console application implementing both spotify and twitter APIs
+public class Twittify{
+    public static void main(String[] args)
+    {
+        Menu menu = new Menu();
+
+        menu.prompt();
+    }
+}
