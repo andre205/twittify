78320943396199d451c5ec7c837e115730918b37
Tyler Andrews <Tyler Andrews>
2017-04-11 13:08:16 -0700

    bash test

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@ echo Twittify
 javac -cp "json.jar" *.java
 
 #Detect OS and run accordingly
-if [ "$(uname)" == "Darwin" ] then
+if [ "$(uname)" == "Darwin" ]; then
     java -cp ".:json.jar" twittify
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ] then
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     java -cp ".:json.jar" twittify
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] then
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     java -cp ".;json.jar" twittify
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ] then
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     java -cp ".;json.jar" twittify
 fi
 
