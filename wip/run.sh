d8f82978f668d43f458617835301f20149de3058
Tyler Andrews <andre205@mail.chapman.edu>
2017-04-12 23:38:46 -0700

    Adds twitter4j resources

--- wip/run.sh
+++ wip/run.sh
@@ -1,5 +1,2 @@
-#Doesn't work -- looking into it
-echo "Running main.java"
-echo
 javac -cp "twitter4j-core-4.0.4.jar" *.java
-java -cp ".;twitter4j-core-4.0.4.jar;" Main
+java -cp ".;twitter4j-core-4.0.4.jar" Twitter
