5e1ce66f7276aeb29c2626ddfc3da979e7ce6bdd
Tyler Andrews <Tyler Andrews>
2017-04-18 13:30:33 -0700

    Jeff help

---
+++ wip.sh
@@ -0,0 +1,2 @@
+javac -cp ".;main;groundwork;packages/json/json.jar;packages/twitter4j/twitter4j-core.jar" wip/*.java
+java -cp ".;main;groundwork;packages/json/json.jar;packages/twitter4j/twitter4j-core.jar" Twitter
--- wip/run.sh
+++
@@ -1,2 +0,0 @@
-javac -cp "twitter4j-core-4.0.4.jar" *.java
-java -cp ".;twitter4j-core-4.0.4.jar" Twitter
