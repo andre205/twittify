3c4973d9631a98dca095115c7dd31cc1326dea67
haske107 <haske107@mail.chapman.edu>
2017-04-20 02:05:52 -0700

    integrated Spotify and Twitter, Twittify has been born

--- src/twittify.java
+++ src/twittify.java
@@ -1,12 +1,15 @@
 /*
 Console application implementing both spotify and twitter APIs
 */
-
+import java.util.LinkedList;
+import java.util.List;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
-public class twittify{
-    public static void main(String[] args)
+
+public class twittify {
+
+    public static void main (String[] args) throws Exception
     {
         Menu menu = new Menu();
         spotify_groundwork sg = new spotify_groundwork();
@@ -19,12 +22,37 @@ public class twittify{
         {
             spotify_chart_fetcher scf = new spotify_chart_fetcher();
             String[] top200 = scf.getTop200();
-
+            //putting the name and result into table
+            //format so that they can be indexed by number(String)
+            List<String[]> top200table = new LinkedList<String[]>();
+            for(int i = 0; i < 200; ++i)
+            {
+              top200table.add(new String[] {String.valueOf(i+1),top200[i]});
+            }
             System.out.println("Spotify Top 200");
             for (int i = 0; i < 200; ++i)
             {
                 System.out.println((i+1) + ": " + top200[i]);
             }
+            System.out.println("\nEnter song number to pull up most recent " +
+                                                            "related tweets!");
+            String input1 = new java.util.Scanner(System.in).nextLine();
+            for (String[] a : top200table)
+            {
+
+                if(a[0].equals(input1))
+                {
+                  System.out.println("----TWEETS----\n");
+                  twitter_groundwork instance = new twitter_groundwork();
+
+                  for(String b : instance.search(a[1]))
+                  {
+                    System.out.println(b+"\n");
+                  }
+                  String input2 = new java.util.Scanner(System.in).nextLine();
+                }
+            }
+            String input3 = new java.util.Scanner(System.in).nextLine();
         }
 
         else
