1551272034fc8ddd7339b0b4d2914643cce7d846
Tyler Andrews <andre205@mail.chapman.edu>
2017-04-19 22:10:36 -0700

    repo update

--- wip.sh
+++
@@ -1,2 +0,0 @@
-javac -cp ".;main;groundwork;packages/json/json.jar;packages/twitter4j/twitter4j-core.jar" wip/*.java
-java -cp ".;main;groundwork;packages/json/json.jar;packages/twitter4j/twitter4j-core.jar" Twitter
