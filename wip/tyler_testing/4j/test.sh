e13caa49e685f888390faf23640afc627109ae0c
Tyler Andrews <andre205@mail.chapman.edu>
2017-05-09 02:45:25 -0700

    Removes WIP folder

--- wip/tyler_testing/4j/test.sh
+++
@@ -1,2 +0,0 @@
-javac -cp ".;t-a.jar;t-c.jar;t-e.jar;t-m.jar;t-s.jar;" *.java
-java -cp ".;t-a.jar;t-c.jar;t-e.jar;t-m.jar;t-s.jar;" workplease
--- wip/tyler_testing/completegui/mac.sh
+++
@@ -1,2 +0,0 @@
-javac -cp ".:json/json.jar:csv/commons-csv-1.4.jar:twitter4j/twitter4j-core-4.0.4.jar:" *.java
-java -cp ".:json/json.jar:csv/commons-csv-1.4.jar:twitter4j/twitter4j-core-4.0.4.jar:" GUI
