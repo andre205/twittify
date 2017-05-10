252b550d318ad17076c2605829cfb063011cceac
Tyler Andrews <andre205@mail.chapman.edu>
2017-05-09 23:31:06 -0700

    Scripts and README finalized

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 cd src
 echo Compiling...
 javac -cp ".;json/json.jar;csv/commons-csv-1.4.jar;twitter4j/twitter4j-core-4.0.4.jar;" *.java
-echo Running Twittify
+echo Launching Twittify...
 java -cp ".;json/json.jar;csv/commons-csv-1.4.jar;twitter4j/twitter4j-core-4.0.4.jar;" twittify
-echo Twittify now running
